107cbb0cca1403aa685b7f8855d4d161aed39120
Christian Fryksten <christianfryksten1@gmail.com>
2024-11-12 14:12:33 +0100

    API Fetch

--- script.js
+++ script.js
@@ -5,3 +5,13 @@ beer.addEventListener("click", giveBeer);
 function giveBeer() {
   alert("Släpper en fis");
 }
+
+(async () => {
+  const res = await fetch(
+    "https://lernia-sjj-assignments.vercel.app/api/challenges"
+  );
+  const data = await res.json();
+  data.challenges.forEach((challenge) => {
+    console.log(challenge.title);
+  });
+})();
